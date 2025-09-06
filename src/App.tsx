5e314559d38be7b92d74f2c525afcba382edf866
Aasrith Ravulapati <aasrith.rp@gmail.com>
2025-09-06 13:36:48 -0400

    yay

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Library from "./pages/Library";
 import NotFound from "./pages/NotFound";
+import PDFLanguageDetector from "./pages/PDFLanguageDetector";
 
 const queryClient = new QueryClient();
 
@@ -18,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/library" element={<Library />} />
+          <Route path="/pdf-language" element={<PDFLanguageDetector />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
